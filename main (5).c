56f9200da999306e575998680f930713ffe0c631
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-07 18:04:12 -0300

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+ main()
+{
+    
+    int num[10];
+    int guarda_maior;
+    int i;
+
+     printf("Entre com o primeiro numero inteiro:\n");
+     scanf("%d",&num[i]); 
+     
+ for(i=1; i<10; i++)
+ {
+
+ if(num>guarda_maior)
+ guarda_maior=num;
+ 
+} 
+     printf("\nO maior numero entrado e: %d", guarda_maior); 
+    
+}
